314e63f5dad419716847eb5d28b542beacc892d0
vikingasia <vikingasia@users.noreply.github.com>
2014-03-16 17:18:15 -0400

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= 'gU9lB10Qbpdro6jHJNHSGzn15m4xrgtnGaUUs333tAiO0IyO', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1988668865], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Bills Max31855 JSapp', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
